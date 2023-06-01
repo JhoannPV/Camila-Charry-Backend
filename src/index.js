827cd966cf0230c034e86052093f44f5b6882f3a
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2023-05-31 23:20:24 -0500

    users signIn sigOut

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const v1Plantilla_ProductoRouter = require("./v1/routes/plantilla_productoRoutes");
 const v1Plantilla_InsumoRouter = require("./v1/routes/plantilla_insumoRoutes");
+const v1AuthRouter = require("./v1/routes/authRoutes");
 
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use("/api/v1/plantillas-productos", v1Plantilla_ProductoRouter);
 app.use("/api/v1/plantillas-insumos", v1Plantilla_InsumoRouter);
+app.use("/api/v1/auth", v1AuthRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
